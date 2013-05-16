f621a6e4ebcdaf1aba03c038b9e7ec75f3b2a932
Justin Harbour <jandjcds@gmail.com>
2013-05-16 12:10:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,18 +11,18 @@
 sudo apt-get update
 
 #FTP
-sudo apt-get install proftpd
+sudo apt-get install -y proftpd
 
 #Apache
-sudo apt-get install apache2
+sudo apt-get install -y apache2
 sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
 #PHP
-sudo apt-get install php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
+sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
 
 #MySQL
-sudo apt-get install mysql-server mysql-client php5-mysql
+sudo apt-get install -y mysql-server mysql-client php5-mysql
 
 #Additional Dependencies
-sudo apt-get install nmap zenmap
+sudo apt-get install -y nmap zenmap
 
